faa01c5c2fb393b64559cb15a3e236ef13901346
Meezar-Alali <125415480+Meezar-Alali@users.noreply.github.com>
2023-03-12 00:40:48 +0100

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def scale(payload):
 
 @app.route("/")
 def home():
-    html = "<h3>Sklearn Prediction Pipeline</h3>"
+    html = "<h3>Sklearn Prediction CD-CI</h3>"
     return html.format(format)
 
 # TO DO:  Log out the prediction value
